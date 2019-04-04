5f238bacc241e4691321ff200c1985eef23a80c0
Ukhu <osaukhu.bi@gmail.com>
2019-04-04 07:25:41 +0100

    chore(tests):write unit tests for POST /auth/signup api endpoint [Start #165086313]

--- app.js
+++ app.js
@@ -2,8 +2,63 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Your app has started!!');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const users = [];
+
+app.post('/api/v1/auth/signup', (req, res) => {
+  const id = Math.floor(Math.random() * 1001);
+
+  let errMsg;
+
+  // create new user
+  const newUser = {
+    id,
+    email: req.body.email,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    password: req.body.password,
+    type: req.body.type,
+    isAdmin: req.body.isAdmin,
+  };
+
+  // console.log(newUser);
+
+  Object.keys(newUser).forEach((key) => {
+    if (newUser[key] === '' || newUser[key] === undefined) {
+      errMsg = 'Fill all the required fields';
+    }
+  });
+
+  if (errMsg) {
+    res.status(400).json({
+      status: 400,
+      error: errMsg,
+    });
+  } else {
+    // add the user to the database
+    users.push(newUser);
+
+    // console.log(users);
+
+    // create token
+    const token = jwt.sign(newUser, 'examplesecretword');
+
+    res.status(201).json({
+      status: 201,
+      data: {
+        token,
+        id: newUser.id,
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        email: newUser.email,
+      },
+    });
+  }
 });
 
 app.listen(3000, () => console.log('Server Has Started!!!'));
