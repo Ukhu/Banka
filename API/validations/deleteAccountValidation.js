0103e275a4a793d3ca7e4ab8acd26ef299e586ed
Ukhu <osaukhu.bi@gmail.com>
2019-04-19 12:28:08 +0100

    chore(validations):add validations for route parameters
    [Finishes #165465538]

--- API/validations/deleteAccountValidation.js
+++ API/validations/deleteAccountValidation.js
@@ -1,12 +1,12 @@
-import { check, param } from 'express-validator/check';
+import { param } from 'express-validator/check';
 
 /**
-* Adds validations to the transaction routes
+* Adds validations to the delete account route
 * @returns {array} an array containing the Check API validations
 * @memberof Validation
 */
 
-const transactionValidation = () => [
+const deleteAccountValidation = () => [
   param('accountNumber')
     .exists().withMessage('No account number provided in route parameters')
     .not()
@@ -16,14 +16,6 @@ const transactionValidation = () => [
     .withMessage('Account number must be a number')
     .isLength({ min: 7, max: 7 })
     .withMessage('Account number must be 7 digits'),
-  check('amount')
-    .exists().withMessage('No amount specified')
-    .not()
-    .isEmpty({ ignore_whitespace: true })
-    .withMessage('Amount cannot be blank')
-    .isFloat()
-    .withMessage('Amount must be a Floating point number')
-    .trim(),
 ];
 
-export default transactionValidation;
+export default deleteAccountValidation;
